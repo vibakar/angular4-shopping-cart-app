473fd99a908291dc757f94096938235488227639
vibakar <vibakar23@gmail.com>
2018-02-10 11:34:13 +0530

    Filtering the order by status is implemented

--- src/app/admin/components/admin-orders/admin-orders.component.ts
+++ src/app/admin/components/admin-orders/admin-orders.component.ts
@@ -1,22 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { OrderService } from 'shared/services/order.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
   styleUrls: ['./admin-orders.component.css']
 })
-export class AdminOrdersComponent implements OnInit {
-  orders$;
+export class AdminOrdersComponent implements OnInit, OnDestroy {
+  orders;
+  filteredOrders;
+  status:string = 'all';
+  subscription:Subscription;
   constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
-  	this.orders$ = this.orderService.getOrders();
+  	this.subscription = this.orderService.getOrders().subscribe(o=>{
+      this.orders = o;
+      this.filteredOrders = o;
+    });
   }
 
   viewOrder(orderId){
   	this.router.navigate(['/orders', orderId]);
+  }
+
+  processShipping(orderId){
+    this.orderService.updateOrderStatus(orderId);
+  }
+
+  filterByStatus(status) {
+    if(status === 'all'){
+      this.filteredOrders = this.orders;
+    } else {
+      this.filteredOrders = this.orders.filter((order=>{
+       return order.status.toLowerCase() === status;
+     }))
+    }
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
