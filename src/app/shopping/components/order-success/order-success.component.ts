4890998c26bb5acd53ff5128e315a981cc7c9683
vibakar <vibakar23@gmail.com>
2018-02-13 23:09:34 +0530

    order success page done

--- src/app/shopping/components/order-success/order-success.component.ts
+++ src/app/shopping/components/order-success/order-success.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-success',
   templateUrl: './order-success.component.html',
   styleUrls: ['./order-success.component.css']
 })
-export class OrderSuccessComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class OrderSuccessComponent {
+  orderId:string;
+  constructor(private route:ActivatedRoute, private router: Router) {
+  	this.orderId = this.route.snapshot.paramMap.get('id');
   }
 
+  orderDetails(){
+  	this.router.navigate(['/my/orders/', this.orderId]);
+  }
 }
